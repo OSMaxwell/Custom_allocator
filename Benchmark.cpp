82a1819eac67b61f8194c3c78db538480a12d833
Oussama Sayari <oussama_sayari@yahoo.fr>
2021-02-19 13:44:11 +0100

    Updated benchmark script

--- Benchmark.cpp
+++ Benchmark.cpp
@@ -5,49 +5,101 @@
 // Bench results found in git.
 
 #include <list>
-#include <memory_resource>
+#include <vector>
+#include <memory_resource> //C++17
 
-static void pmr_list(benchmark::State& state) {
-  for (auto _ : state) {
+/*static void pmr_list(benchmark::State& state){
+  for (auto _:state){
     std::byte stackBuf[2048];
     std::pmr::monotonic_buffer_resource rsrc(stackBuf, sizeof stackBuf);
-    std::pmr::list<int> list1(
-        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, &rsrc);
-    std::pmr::list<int> list2(
-        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, &rsrc);
+    std::pmr::list<int> list1({1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10},&rsrc);
+    std::pmr::list<int> list2({1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10},&rsrc);
   }
 }
-// Benchmark this function
+//Benchmark this function 
 BENCHMARK(pmr_list);
 
-static void RegularList(benchmark::State& state) {
-  for (auto _ : state) {
-    std::list<int> list3{1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                         1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::list<int> list4{1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                         1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  }
-}
-BENCHMARK(RegularList);
 
-static void std_vector_nopush(benchmark::State& state) {
-  for (auto _ : state) {
-    std::vector<int> vec1{1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                          1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::vector<int> vec2{1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                          1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+static void std_list(benchmark::State& state){
+  for (auto _:state){
+    std::list<int> list3{1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10};
+    std::list<int> list4{1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10};
   }
 }
-BENCHMARK(std_vector_nopush);
+BENCHMARK(std_list); */
 
-static void pmr_vector(benchmark::State& state) {
-  for (auto _ : state) {
-    std::byte stackBuf[2048];
-    std::pmr::monotonic_buffer_resource rsrc(stackBuf, sizeof stackBuf);
-    std::pmr::vector<int> vec1{
-        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, &rsrc};
-    std::pmr::vector<int> vec2{
-        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, &rsrc};
+/*static void std_vector_nopush(benchmark::State& state){
+  for (auto _:state){
+    std::vector<int> vec1{1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10};
+    std::vector<int> vec2{1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10};
   }
 }
-BENCHMARK(pmr_vector);
+BENCHMARK(std_vector_nopush); */
+
+/*static void pmr_vector_nopush(benchmark::State& state){
+    for (auto _:state){
+      std::byte stackBuf[2048];
+      std::pmr::monotonic_buffer_resource rsrc(stackBuf, sizeof stackBuf);
+      std::pmr::vector<int> vec1{{1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10},&rsrc};
+  }
+}
+BENCHMARK(pmr_vector_nopush); */
+
+/*static void std_vector_push(benchmark::State& state){
+  for (auto _:state){
+    std::vector<int> vec1{1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10};
+    std::vector<int> vec2{1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10};
+
+    vec1.push_back(10);
+    vec1.push_back(10);
+
+    vec1.pop_back();
+    vec1.pop_back();
+
+
+    vec2.push_back(10);
+    vec2.push_back(10);
+
+    vec2.pop_back();
+    vec2.pop_back();
+
+
+  }
+}
+BENCHMARK(std_vector_push); */
+
+/*static void pmr_vector_push(benchmark::State& state){
+    for (auto _:state){
+      std::byte stackBuf[2048];
+      std::pmr::monotonic_buffer_resource rsrc(stackBuf, sizeof stackBuf);
+      std::pmr::vector<int> vec1{{1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10},&rsrc};
+
+    vec1.push_back(10);
+    vec1.push_back(10);
+
+    vec1.pop_back();
+    vec1.pop_back();
+
+  }
+}
+BENCHMARK(pmr_vector_push); */
+
+
+/* static void pmr_vector_access(benchmark::State& state){
+    for (auto _:state){
+      std::byte stackBuf[2048];
+      std::pmr::monotonic_buffer_resource rsrc(stackBuf, sizeof stackBuf);
+      std::pmr::vector<int> vec1{{1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10},&rsrc};
+      vec1[5]= 20;
+
+  }
+}
+BENCHMARK(pmr_vector_access);  */
+
+/* static void std_vector_access(benchmark::State& state){
+  for (auto _:state){
+    std::vector<int> vec1{1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10};
+    vec1[5]= 20;
+  }
+}
+BENCHMARK(std_vector_access);  */
